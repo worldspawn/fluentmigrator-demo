f3ff36ed022e504c4175c336335f742fd3d4049f
Sam Critchley <critchley.sj@gmail.com>
2012-06-29 15:56:18 +1000

    more migrations

---
+++ Demo.Data/Migrations/004_ExtendMessageTable.cs
@@ -0,0 +1,13 @@
+﻿using FluentMigrator;
+
+namespace Demo.Data.Migrations
+{
+    [Migration(20120629154244)]
+    public class ExtendMessageTable : AutoReversingMigration
+    {
+        public override void Up()
+        {
+            Create.Column("Text").OnTable("Message").AsString(int.MaxValue).NotNullable();
+        }
+    }
+}
---
+++ Demo.Data/Migrations/005_InsertSomeUsers.cs
@@ -0,0 +1,15 @@
+﻿using FluentMigrator;
+
+namespace Demo.Data.Migrations
+{
+    [Migration(20120629154502)]
+    public class InsertSomeUsers : AutoReversingMigration
+    {
+        public override void Up()
+        {
+            base.Insert.IntoTable("User")
+                .Row(new {Username = "sam.critchley", Password = "password", Email = "fauxemail@gmail.com"})
+                .Row(new {Username = "demo.user", Password = "password", Email = "demo@gmail.com"});
+        }
+    }
+}
